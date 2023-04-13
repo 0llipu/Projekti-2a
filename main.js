3d153d8b40dcf796e81c25a56f08f26f48119a1d
Olli Puustinen <olli.puustinen@outlook.com>
2023-04-13 07:01:57 +0300

    Fixed an error for when entering invalid city to get the coordinates to refresh when entering valid city.

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ input.addEventListener('submit', (e) => {
 	}
 });
 
-async function geoFindMe(pLocation) {
+async function geoFindMe() {
 	function success(position) {
 		const latitude = position.coords.latitude;
 		const longitude = position.coords.longitude;
@@ -89,7 +89,7 @@ async function checkLocation(city) {
 
 	locationRequest.send();
 
-	async function getLocationInfo(pLocation) {
+	async function getLocationInfo() {
 		const lat = locationData[0].lat;
 		const lon = locationData[0].lon;
 
@@ -334,11 +334,6 @@ function clearForecast() {
 }
 
 function clearCurrent() {
-	let location = document.querySelector('#location');
-
-	location.innerHTML = `<h1>Location coordinates</h1>
-                    <p>No Coordinates</p>`;
-
 	let currentWeatherInfo = document.querySelector('#weather');
 
 	currentWeatherInfo.innerHTML = `<div class="current">
